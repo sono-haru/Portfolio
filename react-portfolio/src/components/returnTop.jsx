575ede579759d77e0811fbb4adc5ca88e966e298
sono-haru <mit2471549@stu.o-hara.ac.jp>
2025-05-19 00:15:53 +0900

    ボタンのクリック判定範囲の修正

--- react-portfolio/src/components/returnTop.jsx
+++ react-portfolio/src/components/returnTop.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 function ReturnTop() {
   return (
     <div className="flex justify-center">
-      <div className="inline-block bg-blue-300 text-[#FEFBF4] py-1 md:py-2 px-6 md:px-8 rounded-full mb-4 md:mb-6 hover:opacity-95 hover:scale-[99%] active:scale-[96%] transition-transform ease-in-out duration-200">
-        <a href="/" className='text-sm md:text-base'>戻る</a>
-      </div>
+      <a
+        href="/"
+        className="inline-block bg-blue-300 text-[#FEFBF4] py-1 md:py-2 px-6 md:px-8 rounded-full mb-4 md:mb-6 hover:opacity-95 hover:scale-[99%] active:scale-[96%] transition-transform ease-in-out duration-200 text-sm md:text-base"
+      >
+        戻る
+      </a>
     </div>
   )
 }
